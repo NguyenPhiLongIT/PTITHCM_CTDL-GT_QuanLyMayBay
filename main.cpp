fe9850629d230ab2fcc84b6ab778d4b1f54781dd
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2023-03-30 14:52:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,55 @@
 #include "Airplane.h"
 #include "Flight.h"
 #include "UserInterface.h"
+#include "Passenger.h"
+#include "KeyValue.h"
 
 using namespace std;
 
+void CreateForm(string content[], int StartIndex, int nContent, int length);
+
 void menu_xuli();
 
+// void Duyet(TreePass root){
+//     if(root != NULL){
+//         ShowPass(root->data);
+//         Duyet(root->pLeft);
+//         Duyet(root->pRight);
+//     }
+// }
+
 int main(){
-	fullScreen();
+        
+    // ShowCursor(false); // ?n con tr?(false)/ hi?n(true);
+    // SetConsoleOutputCP(65001); //Set b?ng mã kí t? d? in ra ti?ng vi?t ...
+    // SetConsoleCP(65001); //Set nh?p vào ti?ng vi?t ...
+    // SetConsoleTitleW(L"Qu?n lí máy bay"); //Set Title for console
+    // cout << "Xin chào m?i ngu?i mình tên là Chó Thùy" << endl;
+    // clreol({0,0});
+    // cout << "Xin chào m?i ngu?i mình tên là Chó Thùy" << endl;
+    // wchar_t ch;
+    // wcin >> ch;
+    // cout << ch << endl;
+
+    // TreePass treePass;
+    // InitTreePass(treePass);
+    // Passenger pass[] = {
+    //     {"12345", "Nguyen Tan", "Nguyen", 1},
+    //     {"53232", "Vo Bich", "Thi", 0},
+    //     {"63323", "Phan Thi My", "Duyen", 0}
+    // };
+    // InsertPass(treePass, pass[0]);
+    // InsertPass(treePass, pass[1]);
+    // InsertPass(treePass, pass[2]);
+    // Duyet(treePass);
+//     char r[100] = {};
+//     strcpy(r,Input(100, ID));
+//     cout << endl << r << endl;
+    
+//     _getch();
+    
+    fullScreen();
     menu_xuli();
-	
     return 0;
 }
 
@@ -27,6 +67,7 @@ void menu_xuli() {
     InitTicketList(ticketlist);
     Ticket ticket;
     InputListAirplane(list);
+    system("cls");
     while (true) {
         menuchinh:
         gotoxy(X_TitlePage-1,Y_TitlePage-1);
@@ -46,9 +87,12 @@ void menu_xuli() {
             case 4: // Passenger
                 break;
             case 5: // Ticket
-                MenuManagaTicket(ticketlist,ticket);
-                system("cls");
-                goto menuchinh;
+            {
+//                MenuManagaTicket(ticketlist,ticket);
+//                system("cls");
+//                goto menuchinh;
+					break;
+            }
             default: //Exit
                 return;
         }
