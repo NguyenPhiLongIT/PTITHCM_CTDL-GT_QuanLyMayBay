50e04f24891b8fddfc6d40bf28c35afa98a20b3e
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2023-03-30 16:09:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string.h>
+
+using namespace std;
 #include "Date.h"
 #include "Mylibrary.h"
 #include "Constraint.h"
@@ -10,50 +12,13 @@
 #include "Passenger.h"
 #include "KeyValue.h"
 
-using namespace std;
 
 void CreateForm(string content[], int StartIndex, int nContent, int length);
 
 void menu_xuli();
 
-// void Duyet(TreePass root){
-//     if(root != NULL){
-//         ShowPass(root->data);
-//         Duyet(root->pLeft);
-//         Duyet(root->pRight);
-//     }
-// }
-
 int main(){
-        
-    // ShowCursor(false); // ?n con tr?(false)/ hi?n(true);
-    // SetConsoleOutputCP(65001); //Set b?ng mã kí t? d? in ra ti?ng vi?t ...
-    // SetConsoleCP(65001); //Set nh?p vào ti?ng vi?t ...
-    // SetConsoleTitleW(L"Qu?n lí máy bay"); //Set Title for console
-    // cout << "Xin chào m?i ngu?i mình tên là Chó Thùy" << endl;
-    // clreol({0,0});
-    // cout << "Xin chào m?i ngu?i mình tên là Chó Thùy" << endl;
-    // wchar_t ch;
-    // wcin >> ch;
-    // cout << ch << endl;
 
-    // TreePass treePass;
-    // InitTreePass(treePass);
-    // Passenger pass[] = {
-    //     {"12345", "Nguyen Tan", "Nguyen", 1},
-    //     {"53232", "Vo Bich", "Thi", 0},
-    //     {"63323", "Phan Thi My", "Duyen", 0}
-    // };
-    // InsertPass(treePass, pass[0]);
-    // InsertPass(treePass, pass[1]);
-    // InsertPass(treePass, pass[2]);
-    // Duyet(treePass);
-//     char r[100] = {};
-//     strcpy(r,Input(100, ID));
-//     cout << endl << r << endl;
-    
-//     _getch();
-    
     fullScreen();
     menu_xuli();
     return 0;
@@ -67,6 +32,7 @@ void menu_xuli() {
     InitTicketList(ticketlist);
     Ticket ticket;
     InputListAirplane(list);
+	
     system("cls");
     while (true) {
         menuchinh:
