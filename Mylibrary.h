4dddfce85c3a77acadcab27223a808f87d0a9841
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2023-04-10 19:03:02 +0700

    Update Mylibrary.h

--- Mylibrary.h
+++ Mylibrary.h
@@ -70,7 +70,7 @@ int wherey()
     GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
     return screen_buffer_info.dwCursorPosition.Y;
 }
-// Sử dụng để xóa dòng từ vị trí coord đến con trỏ hiện tại trên Console
+// Sá»­ dá»¥ng Ä‘á»ƒ xÃ³a dÃ²ng tá»« vá»‹ trÃ­ coord Ä‘áº¿n con trá» hiá»‡n táº¡i trÃªn Console
 void clreol(COORD coord)
 {
     // COORD coord;
@@ -105,6 +105,17 @@ void SetColor(WORD color)
     SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
 
+void GetColor(WORD &color)
+{
+	HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+    
+    color = screen_buffer_info.wAttributes;
+}
+
 void SetBGColor(WORD color)
 {
     HANDLE hConsoleOutput;
@@ -121,15 +132,23 @@ void SetBGColor(WORD color)
 
     SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
-//Xóa toàn bộ màn hình console
+//XÃ³a toÃ n bá»™ mÃ n hÃ¬nh console
 void Clear()
 {
-    HANDLE hConsoleOutput;
-    COORD Position;
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-    Position.X = 0;
-    Position.Y = 0;
-    SetConsoleCursorPosition(hConsoleOutput, Position);
+	SetBGColor(0);
+    DWORD written;
+    COORD coord;
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+    coord.X = 0;
+    coord.Y = 0;
+    FillConsoleOutputCharacter(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        ' ',
+        info.dwSize.X - info.dwCursorPosition.X * info.dwCursorPosition.Y,
+        coord,
+        &written
+    );
 }
 
 //Hien thi con tro hoac khong
@@ -158,4 +177,3 @@ void fullScreen()
 	keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
 	keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
 }
-
