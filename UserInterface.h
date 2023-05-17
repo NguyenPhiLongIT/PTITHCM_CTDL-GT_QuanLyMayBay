920bad13bf2714447f484d1bfcdf13f3abdc73e7
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2023-05-17 07:16:19 +0700

    Update UserInterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -12,27 +12,34 @@ using namespace std;
 
 // Xuat bang thong tin
 int xKeyDisplay[8] = {1,19,37,55,75,93,111,127};
-//Airplane
+int CurPage = 1;
+int TotalPage = 0;
+
+//May bay
 string ContentAirplane[5] = { "Plane ID", "Plane Type", "Column", "Row", "Seat"};
-string ContentThaoTac[6] = {"Insert", "Delete", "Edit", "Previous Page", "Next Page", "Back"};
-string ContentAirplane_Input[3] = {"Plane ID", "Plane Type", "Column & Row"};
-//Flight
+string ContentAirSelect[6] = {"Insert", "Delete", "Edit", "Previous Page", "Next Page", "Back"};
+string ContentAirplaneInput[3] = {"Plane ID", "Plane Type", "Column & Row"};
+
+//Chuyen bay
 string ContentFlight[6] = { "Flight ID", "Destination", "Plane ID", "Date and Time", "Number Tickets", "Status"};
-string ContentFlight_ThaoTac[6] = {"Insert", "Edit DateTime", "Cancle Flight", "Previous Page", "Next Page", "Back"};
-//Ticket
-string ContentTicket_Input[2] = {"PassengerID","Seat"};
-string ContentTicket_Output[3] = {"Seat", "Status", "PassengerID"};
-string ContentTicket_ThaoTac[5] = {"Book Ticket", "Filter", "Previous Page", "Next Page", "Back"};
-//Passenger
+string ContentFlightSelect[7] = {"Insert", "Edit DateTime", "Cancle Flight", "NumberOfPass", "Previous Page", "Next Page", "Back"};
+
+//Ve
+string ContentTicketInput[2] = {"PassengerID","Seat"};
+string ContentTicketOutput[3] = {"Seat", "Status", "PassengerID"};
+string ContentTicketSelect2[5] = {"Book Seat", "Cancel Seat", "Previous Page", "Next Page", "Back"};
+string ContentTicketSelect1[5] = {"Book Ticket", "Filter", "Previous Page", "Next Page", "Back"};
+
+//Hanh khach
 string ContentPass[4] = {"ID", "FirstName", "LastName", "Gender"};
-string ContentPass_ThaoTac[3] = {"Insert", "Next Page", "Back"};
+string ContentPassSelect[4] = {"Insert", "Previous Page", "Next Page", "Back"};
 string ContentGender[2] = {"Nam", "Nu"};
+
 //Menu
 string ContentMenu[6] = {"Home", "Plane", "Flight", "Passenger", "Ticket", "Exit"};
 
-
-
-void box(int x, int y, int w, int h, string nd) {
+//Ve 1 hop thoai
+void Box(int x, int y, int w, int h, string nd) {
 	WORD curColor;
 	GetColor(curColor);
 	SetBGColor(BLACK);
@@ -69,7 +76,8 @@ void box(int x, int y, int w, int h, string nd) {
 	SetColor(curColor);
 }
 
-void remove_box(int x, int y, int w, int h) {
+//Xoa 1 hop thoai
+void RemoveBox(int x, int y, int w, int h) {
 	for (int iy = y; iy <= y+h; iy++) {
 		for (int ix = x; ix <= x + w; ix++) {
 			gotoxy(ix,iy);
@@ -78,23 +86,25 @@ void remove_box(int x, int y, int w, int h) {
 	}
 }
 
+//Thong bao
 void Notification(string s) {
 	ShowCursor(false);
 	int n = s.length();
 	int x = wherex(), y = wherey();
-	box(X_Notification,Y_Notification+1,n+1,2,s);
-	Sleep(1200);
+	Box(X_Notification,Y_Notification+1,n+1,2,s);
+	Sleep(1700);
 	while(_kbhit()) _getch(); //khong nhan phim trong thoi gian ngu
-	remove_box(X_Notification,Y_Notification+1,n+1,2);
+	RemoveBox(X_Notification,Y_Notification+1,n+1,2);
 	ShowCursor(true);
 }
 
-void n_box(int x, int y, int soluong, string noidung[]) {
+//Tao nhieu hop thoai
+void MultiBox(int x, int y, int soluong, string noidung[]) {
 	WORD curColor;
 	GetColor(curColor);
 	for (int i = 0; i < soluong; i++) {
 		SetColor(YELLOW);
-		box(x, y + i*Box_Height, Box_Width, Box_Height, noidung[i]);
+		Box(x, y + i*Box_Height, Box_Width, Box_Height, noidung[i]);
 		if (i != 0) {
 			gotoxy(x, y + i*Box_Height);
 			cout << char(195);
@@ -105,7 +115,8 @@ void n_box(int x, int y, int soluong, string noidung[]) {
 	SetColor(curColor);
 }
 
-void thanh_sang(int x, int y, int w, int h, int b_color, string nd) {
+//Thanh sang
+void Highlight(int x, int y, int w, int h, int b_color, string nd) {
 	WORD curColor;
 	GetColor(curColor);
 	ShowCursor(false);
@@ -123,19 +134,19 @@ void thanh_sang(int x, int y, int w, int h, int b_color, string nd) {
 	SetColor(curColor);
 }
 
-int menu_dong(int x, int y, int sl, string nd[]) {
-	n_box(x,y,sl,nd);
-	int xp = x, yp = y;
-	int xcu = xp, ycu = yp;
+int MenuSelect(int x, int y, int sl, string nd[]) {
+	MultiBox(x,y,sl,nd);
+	int xtmp = x, ytmp = y;
+	int xOld = xtmp, yOld = ytmp;
 	int i = 0;
 	int j = i; //luu vi tri box gan nhat
 	bool kt = true; //kiem tra co nhap phim khong
 	while (true) {
 		if (kt == true) {
-			gotoxy(xcu,ycu);
-			thanh_sang(xcu,ycu,Box_Width,Box_Height,BLACK,nd[j]);
-			j = i; xcu = xp; ycu = yp;
-			thanh_sang(xp,yp,Box_Width,Box_Height,BLUE_LIGHT,nd[i]);
+			gotoxy(xOld,yOld);
+			Highlight(xOld,yOld,Box_Width,Box_Height,BLACK,nd[j]);
+			j = i; xOld = xtmp; yOld = ytmp;
+			Highlight(xtmp,ytmp,Box_Width,Box_Height,BLUE_LIGHT,nd[i]);
 			kt = false;
 		}
 		if (_kbhit()) {
@@ -143,25 +154,26 @@ int menu_dong(int x, int y, int sl, string nd[]) {
 			if (c == -32) {
 				kt = true;
 				c = _getch();
-				if (c == UP && yp != y) {
-					yp -= Box_Height;
+				if (c == UP && ytmp != y) {
+					ytmp -= Box_Height;
 					i--;
-				} else if (c == DOWN && yp != y + Box_Height*(sl-1)) {
-					yp += Box_Height;
+				} else if (c == DOWN && ytmp != y + Box_Height*(sl-1)) {
+					ytmp += Box_Height;
 					i++;
 				} 
 			} else if (c == ENTER) {
-				if (yp == y + Box_Height*(sl-1)) thanh_sang(xp,yp,Box_Width,Box_Height,BLACK,nd[i]);
+				if (ytmp == y + Box_Height*(sl-1)) Highlight(xtmp,ytmp,Box_Width,Box_Height,BLACK,nd[i]);
 				return i+1;
 			} else if (c == ESC) {
-				thanh_sang(xp,yp,Box_Width,Box_Height,BLACK,nd[i]);
+				Highlight(xtmp,ytmp,Box_Width,Box_Height,BLACK,nd[i]);
 				return -1;
 			}
 		}
 	}
 }
 
-void Display(string content[], int nContent) 	// ve bang
+//Ve bang
+void Display(string content[], int nContent) 
 {
 	SetColor(11);
 	SetBGColor(0);
@@ -261,7 +273,7 @@ void RemoveTable(string content[], int nContent){
 		printf("%-16s"," ");
 	}
 
-	//ve cac duong thang de phan chia cac cot - kich thuoc toi da la 28 ve chieu dai
+	//xoa cac duong thang de phan chia cac cot - kich thuoc toi da la 28 ve chieu dai
 	for (int j = Y_Display ; j <= Y_Display + 24; j++)
 	{
 		for (int i = 0; i < nContent+1; i++)
@@ -270,18 +282,18 @@ void RemoveTable(string content[], int nContent){
 			printf("%-20s"," ");
 		}
 	}
-	//ve thanh ngang ben tren va duoi
+	//xoa thanh ngang ben tren va duoi
 	for (int i = xKeyDisplay[0]; i <= xKeyDisplay[nContent]; i++)
 	{
-		//ve thanh ngang ben tren so 1
+		//xoa thanh ngang ben tren so 1
 		gotoxy(i, Y_Display-1);
 		cout << " ";
 
-		// ve thanh ngang ben tren so 2
+		//xoa thanh ngang ben tren so 2
 		gotoxy(i, Y_Display + 2);
 		cout << " ";
 
-		//ve thanh ngang ben duoi
+		//xoa thanh ngang ben duoi
 		gotoxy(i, Y_Display + 25);
 		cout << " ";
 	}
