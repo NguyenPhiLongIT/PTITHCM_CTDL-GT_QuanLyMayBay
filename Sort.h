680645764dd3f9d4a46a813e5451044550574c78
NNNNNguyennnnn <106095525+NNNNNguyennnnn@users.noreply.github.com>
2023-06-05 16:22:28 +0700

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,48 @@
+template<class valueType>
+void Swap(valueType &a, valueType &b){
+    valueType temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template<class valueType>
+bool Asc(valueType &a, valueType &b){
+    return a > b;
+}
+
+template<class valueType>
+bool Desc(valueType &a, valueType &b){
+    return a < b;
+}
+
+template<class valueType>
+void Quicksort(valueType *array, int low, int up, bool (*compare)(valueType &a, valueType &b) = Asc){
+    
+    valueType temp;
+    int left, right;
+
+    //Setting the index
+    left = low;
+    right = up-1;
+    temp = array[(left + right)/2];
+
+    //Loop till pivot is placed at proper place in the sublist
+    while(left < right){
+        while(compare(array[left], temp))
+            ++left;
+        
+        while(compare(temp, array[right]))
+            --right;
+        
+        if(left <= right){
+            Swap(array[left], array[right]);
+            ++left;
+            --right;
+        }
+        if(low < right)
+            Quicksort(array, low, right, compare);
+        if(left < up)
+            Quicksort(array, left, up-1, compare);
+    }
+}
